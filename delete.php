8c6fe23362c8e36a177dcebdb0eb686c4cd88b75
RemRem <remrem@users.noreply.github.com>
2020-02-17 20:05:32 +0100

    petites corrections (#2)
    * petites corrections
    
    N'utilises pas ça en prod, ce n'est pas sécurisé
    
    * Update delete.php
    
    * Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,30 @@
 <?php
-	echo $_POST["file"];
-	$locationfile = "./"+$_SESSION["user"]+"/".$_POST["file"];
-	echo $locationfile;
-	echo $d;
-	if (is_dir($_SESSION["user"]."/".$_POST["file"])) {
+	session_start();
+
+	var_dump($_POST["file"]);
+
+	// affiche le contenu de la session
+	var_dump($_SESSION);
+
+	// récupére le chemin du dossier actuel (celui de ce script)
+	$path = dirname(__FILE__).'/';
+
+	$locationfile = $path.$_SESSION["user"].'/'.$_POST["file"];
+
+	var_dump($locationfile);
+
+	// test si le dossier existe
+	if (!file_exists($locationfile)) {
+		var_dump('Introuvable');
+		// tue le script
+		die();
+	}
+
+	if (is_dir($locationfile)) {
 		rmdir ($_SESSION["user"]."/".$_POST["file"]);
 	}
 	else {
-		unlink($_SESSION["user"]."/".$_POST["file"]);
+		unlink($locationfile);
 	}
 
 	// header() renvoit sur une autre page
