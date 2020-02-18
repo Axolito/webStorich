43465bbfbc275af9cc4d60c0da41746441985a4d
RemRem <remrem@users.noreply.github.com>
2020-02-18 13:41:45 +0100

    modification upload (#3)
    je n'ai pas tout corrigé, mais je t'ai mis sur la piste ;)

--- upload.php
+++ upload.php
@@ -1,5 +1,28 @@
 <?php
+/**
+ * note par RemRem 2020-02-18 01:07
+ *  - j'ai ajouté un sous dossier (lire le code) pour éviter que tu n'écrases par erreur un des scripts que tu as en place
+ *  - il y a une erreur sur la façon de tester les erreurs sur l'envoie de fichier
+ */
+	// récupére le chemin du dossier actuel (celui de ce script)
+	$path = dirname(__FILE__).'/';
+
+	// on vérifie que le dossier existe
+	$systeme_de_dossier_a_prevoir = 'test/';
+	if (!is_dir($path.$systeme_de_dossier_a_prevoir)) {
+		// le dossier n'existe pas, à voir si tu veux le créer
+		var_dump('Le dossier '.$systeme_de_dossier_a_prevoir.' n\'existe pas';)
+		// si tu veux créer le dossier, quelque chose comme ça devrait le faire
+		/*
+		if (!mkdir($path.$systeme_de_dossier_a_prevoir)) {
+			var_dump('Impossible de créer le dossier '.$systeme_de_dossier_a_prevoir;)
+		}
+		*/
+	}
+
+	var_dump($_FILES);
 	if(isset($_FILES['upfile'])){
+		// note de RemRem : tu crées un tableau $errors vide
 		$errors= array();
 		$file_name = $_FILES['upfile']['name'];
 		$file_size = $_FILES['upfile']['size'];
@@ -9,10 +32,16 @@
 		if($file_size > 2097152) {
 			$errors[]='File size must be excately 2 MB';
 		}
-		
+
+		// note de RemRem : tu testes le tableau $errors vide du coup, il ne peux pas y avoir d'erreur
+                //                  regarde plutot du côté de $_FILES['upfile']['error']
+		//                  cf. le var_dump($_FILES); au dessus
 		if(empty($errors)==true) {
-			move_uploaded_file($file_tmp,"".$file_name);
-			echo "Success";
+			if (move_uploaded_file($file_tmp, $path.$systeme_de_dossier_a_prevoir.$file_name)) {
+				echo "Success";
+			} else {
+				echo 'Erreur';
+			}
 		}
 		else{
 			print_r($errors);
